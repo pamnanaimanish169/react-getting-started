2767d41de90c02118750b384d2a409db246694b1
manish <manishpamnani169@gmail.com>
2021-11-04 09:47:54 +0530

    Create Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form } from 'react-bootstrap';
+import   Button   from  'react-bootstrap/Button';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  
+  constructor(props)  {
+    super(props);
+
+    this.state  = {todoValue  : ''};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({todoValue  : event.target.value});
+  }
+
+  handleSubmit(event) {
+    // Stop the form from reloading
+    event.preventDefault();
+
+    console.log(this.state.todoValue);
+
+    // Call the API to save the Todo
+  }
+
+  render()  {
+    return(
+      <div>
+      <Form>
+        
+        <Form.Group className="mb-3"  controlId="formBasicEmail">
+          <Form.Label>Add a todo</Form.Label>
+          <Form.Control type="text"  placeholder="Add a todo" onChange={this.handleChange} value={this.state.todoValue} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+          Submit
+        </Button>
+      </Form>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+export  default App;
